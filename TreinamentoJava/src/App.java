2b209e8c4b735808a13f86d4946c262a66955512
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-08-24 11:17:57 -0300

    Funções matemáticas em Java

--- TreinamentoJava/src/App.java
+++ TreinamentoJava/src/App.java
@@ -8,65 +8,32 @@ public class App {
      */
     public static void main(String[] args) throws Exception {
     System.out.println();
-    // Entrada de dados com Java - Parte 1
-    // usando p Scanner
+    // Funções matematicas com java
+    double x = 3.0;
+    double y = 4.0;
+    double z = -5.0;
+    double A, B, C;
 
-    // Antes de digitar esse número deve ser 
-    // definido a localização
-    Locale.setDefault(Locale.US);
+    //Raiz quadrada de um número.
+    A = Math.sqrt(x);
+    B = Math.sqrt(y);
+    C = Math.sqrt(25.0);
+    System.out.println("Raiz quadada de " + x + " = " + A);
+    System.out.println("Raiz quadada de " + y + " = " + B);
+    System.out.println("Raiz quadada de 25 = " + C);
 
-    Scanner entDados = new Scanner(System.in);
-    
-    // Entrada de dados até a quebra de linha
-    String s1, s2, s3;
+    // Potenciação
+    A = Math.pow(x, y);
+    B = Math.pow(x, 2.0);
+    C = Math.pow(5.0, 2.0);
+    System.out.println(x + " elevado a " + y + " = " + A);
+    System.out.println(x + " elevado ao quadrado = " + B);
+    System.out.println("5 elevado ao quadrado = " + C);
 
-    System.out.print("Primeir linha: ");
-    s1 = entDados.nextLine();
-    System.out.print("Segunda linha: ");
-    s2 = entDados.nextLine();
-    System.out.print("Terceira linha; ");
-    s3 = entDados.nextLine();
-
-    System.out.println("Dados digitados: ");
-    System.out.println(s1);
-    System.out.println(s2);
-    System.out.println(s3);
-    System.out.println();
-
-    //Resolvendo quebra de linha pendente
-    //Gerando o erro.
-    int x;
-    String q1, q2, q3;
-
-    System.out.println("Esperando digitar um valor numerico e 3 pequenos textos.");
-    x = entDados.nextInt();
-    q1 = entDados.nextLine();
-    q2 = entDados.nextLine();
-    q3 = entDados.nextLine();
-
-    System.out.println("Dados digitados:");
-    System.out.println(x);
-    System.out.println(q1);
-    System.out.println(q2);
-    System.out.println(q3);
-    System.out.println();
-
-    // Resolvendo o erro acima
-    System.out.println("Resolvendo o erro acima.");
-    System.out.println("Esperando digitar um valor numerico e 3 pequenos textos.");
-    x = entDados.nextInt();
-    entDados.nextLine();
-    q1 = entDados.nextLine();
-    q2 = entDados.nextLine();
-    q3 = entDados.nextLine();
-    System.out.println();
-
-    System.out.println("Dados digitados após a correção:");
-    System.out.println(x);
-    System.out.println(q1);
-    System.out.println(q2);
-    System.out.println(q3);
-
-    entDados.close();
+    //Valor absoluto
+    A = Math.abs(y);
+    B = Math.abs(z);
+    System.out.println("Valor absoluto de " + y + " = " + A);
+    System.out.println("Valor absoluto de " + z + " = " + B);
     }
 }
