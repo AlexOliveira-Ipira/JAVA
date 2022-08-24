35741dc71123d3a92c4f374267806d539fd28872
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-08-24 17:53:06 -0300

    Funções (sintaxe)

--- TreinamentoJava/src/App.java
+++ TreinamentoJava/src/App.java
@@ -1,4 +1,6 @@
-import java.lang.invoke.StringConcatFactory;
+import java.util.Scanner;
+
+import javax.xml.stream.events.EndDocument;
 
 public class App {
     /**
@@ -7,44 +9,38 @@ public class App {
      */
     public static void main(String[] args) throws Exception {
     System.out.println();
-  
-    //Funções para string
-    String original = "abcde FGHIJ ABC abc DEFG    ";
+    // Funções (sintaxe)
 
-    String s01 = original.toLowerCase();
-    String s02 = original.toUpperCase();
-    String s03 = original.trim();
-    String s04 = original.substring(2);
-    String s05 = original.substring(2, 9);
-    String s06 = original.replace('a', 'x');
-    String s07 = original.replace("abc", "xyz");
-    int i = original.indexOf("bc");
-    int j = original.lastIndexOf("bc");
+    Scanner entDados = new Scanner(System.in);
+
+    System.out.println("Enter three numbers: ");
+    int a = entDados.nextInt();
+    int b = entDados.nextInt();
+    int c = entDados.nextInt();
+
+    int higher = max(a, b, c);
+
+    showResult(higher);
 
 
-    System.out.println("Original: -" + original + "-");
-    System.out.println("toLowerCase: -" + s01 + "-");
-    System.out.println("toUpperCase: -" + s02 + "-");
-    System.out.println("trim: -" + s03 + "-");
-    System.out.println("substring(2): -" + s04 + "-");
-    System.out.println("substring(2,9): -" + s05 + "-");
-    System.out.println("replace('a', 'x'): -" + s06 + "-") ;
-    System.out.println("replace('abc', 'xyz'): -" + s07 + "-");
-    System.out.println("Index of 'bc': " + i);
-    System.out.println("Last index of 'bc': " + j);
+    entDados.close();
+    }
 
-    //Exemplo
-    //Operação split
-    String s = "potato apple lemon";
+    public static int max(int x, int y, int z){
+        int aux;
+        if(x > y && x > z){
+            aux = x;
+        }
+        else if (y > z){
+            aux = y;
+        }
+        else {
+            aux = z;
+        }
+        return aux;
+    }
 
-    String[] vect = s.split(" ");
-   
-    System.out.println();
-    System.out.println("Scting original: " + s);
-    System.out.println();
-    System.out.println("Utilizando os vetores criado pelo split:");
-    System.out.println(vect[0]);
-    System.out.println(vect[1]);
-    System.out.println(vect[2]);
+    public static void showResult(int value){
+        System.out.println("Higher = " + value);
     }
 }
