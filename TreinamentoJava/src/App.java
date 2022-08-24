83b7bcf2d69f25641ac2f102ce1fe45ca930db5d
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-08-24 09:41:24 -0300

    Processamento de  dados em Java, usando o Casting

--- TreinamentoJava/src/App.java
+++ TreinamentoJava/src/App.java
@@ -1,37 +1,96 @@
-import java.util.Locale;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        // Enunciado exercício de fixação.
-        /*Em um novo progrma, inicie as seguintes variáveis */
-        String product1 = "Computer";
-        String product2 = "Office desk";
+    System.out.println();
+    // Processamento de dados com Java
+    // Exemplo 1 - calculando valores inteiros
 
-        int age = 30;
-        int code = 5290;
-        char gender = 'F';
+    int a , b;
 
-        double price1 = 2100.0;
-        double price2 = 650.50;
-        double measure = 53.234567;
+    a = 5;
+    b = 2 * a;
+    
+    System.out.println("Calculo valores inteiro.");
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println();
 
-        /*Em seguida, usando os valores das variávies, prodza a
-         * seguinte saída na tela do console:
-         */
+    // Exemplo 2 - calculando valores inteiros e double
+    
 
-        System.out.println();
-        System.out.println("Products:");
-        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
-        System.out.printf("%s, which price is $ %.2f%n", product2, price2);
-        System.out.println();
-        System.out.printf("Record: %d years old, code %d and gender: %s%n", age, code, gender);
-        System.out.println();
-        System.out.printf("Measue with eight decimal places: " + "%.8f%n", measure);
-        System.out.printf("Rouded (three decimal places):" + "%.3f%n" ,measure);
+    double c;
 
-         //Define a localização do programa, 
-        //colocando o ponto no lugar da virgula. 
-        Locale.setDefault(Locale.US);
-        System.out.printf("US decimal point: " + "%.3f%n" , measure);
+    c = 2 * a;
+    System.out.println("Calculo entre int e double");
+    System.out.println(a);
+    System.out.println(c);
+    System.out.println();
+
+    // Exemplo 3 - Calculando area com dobloe
+
+    double d, e, f, area;
+
+    d = 6.0;
+    e = 8.0;
+    f = 5.0;
+
+    area = (d + e) / 2.0 * f;
+
+    System.out.println("Valor da Area com doble no calculo");
+    System.out.println(area);
+    System.out.println();
+
+     // Exemplo 3 - Calculando area com float
+
+     double df, ef, ff, areaf;
+
+     df = 6f;
+     ef = 8f;
+     ff = 5f;
+ 
+     areaf = (df + ef) / 2.0 * ff;
+ 
+     System.out.println("Valor da Area com float no calculo");
+     System.out.println(areaf);
+     System.out.println();
+
+     // Exemplo 4 - Calculando inteiro e armazenando em uma variavel double
+        
+     int g ,h;
+     double resultado;
+
+     g = 5;
+     h = 2;
+
+     resultado = g / h;
+
+     System.out.println("Valor inteiro armazenado em uma variavel double");
+     System.out.println(resultado);
+
+    // Resolvendo o problema acima com a utilização do cast
+    g = 5;
+    h = 2;
+
+    resultado = (double) g / h;
+
+    System.out.println("Valor inteiro armazenado em uma variavel double");
+    System.out.println("utilizando o cast durante a armazenagem dos dados.");
+    System.out.println(resultado);
+    System.out.println();
+
+    // Exemplo 5
+    double i;
+    int j;
+
+    i = 5.0;
+    j = (int) i;
+    
+    System.out.println("Convertendo de double para int perdendo as casas decimais");
+    System.out.println(j);
+    System.out.println();
+
+    
+
+
+
     }
 }
